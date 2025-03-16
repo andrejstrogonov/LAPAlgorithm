f1c0226be7c3bfebbad437d6a0c89276b3661d1d
andrejstrogonov <strogonovandrej@hotmail.com>
2025-03-16 20:21:58 +0300

    Added new LPStructure code based on scp algorithm

--- scp_final_lpstructure.py
+++ scp_final_lpstructure.py
@@ -1,5 +1,5 @@
 import random
-import math
+
 
 class LPStructure:
     def __init__(self, eLengthItems, eItemBitSize):
@@ -61,6 +61,7 @@ class LPStructure:
         nBit = nAtom % self.eItemBitSize
         nMask = 1 << (self.eItemBitSize - 1 - nBit)
         return bool(eTest[nItem] & nMask)
+
 class LPStructureReduction(LPStructure):
     def __init__(self, eLengthItems, eItemBitSize, prContainer):
         super().__init__(eLengthItems, eItemBitSize)
@@ -133,66 +134,6 @@ class LPStructureReduction(LPStructure):
     def right(self, pair):
         return pair[1]
 
-    def LE(self, ls, rs):
-        return all(l <= r for l, r in zip(ls, rs))
-
-    def lJoin(self, eRes, right):
-        for i in range(len(eRes)):
-            eRes[i] |= right[i]
-
-
-
-class Lattice:
-    def __init__(self, points):
-        self.points = points
-
-    def generate_points(self, num_points):
-        if num_points > len(self.points):
-            raise ValueError("Number of points requested exceeds the total number of available points.")
-
-            # Randomly sample the points
-        sampled_points = random.sample(self.points, num_points)
-
-        return sampled_points
-
-    def calculate_distance(self, point1, point2):
-        distance = math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-        return distance
-
-class FormalContext:
-    def __init__(self, lattice, relations):
-        self.lattice = lattice
-        self.relations = relations  # Dictionary where keys are lattice points and values are sets of attributes
-
-    def derive_concepts(self):
-        concepts = []
-        for point_set in self.generate_power_set(self.lattice.points):
-            intent = self.derive_intent(point_set)
-            if intent:
-                extent = self.derive_extent(intent)
-                if extent:
-                    concepts.append((extent, intent))
-        return concepts
-
-    def derive_intent(self, point_set):
-        intent = set()
-        for point in point_set:
-            intent.update(self.relations[point])
-        return intent
-
-    def derive_extent(self, intent):
-        extent = set()
-        for point in self.lattice.points:
-            if intent.issubset(self.relations[point]):
-                extent.add(point)
-        return extent
-
-    def generate_power_set(self, s):
-        power_set = [[]]
-        for elem in s:
-            power_set.extend([subset + [elem] for subset in power_set])
-        return power_set
-
 def generate_initial_population(size, num_subsets):
     population = []
     for _ in range(size):
@@ -220,8 +161,6 @@ def crossover(parent1, parent2, crossover_rate, lattice):
         point = random.randint(1, len(parent1) - 1)
         child1 = parent1[:point] + parent2[point:]
         child2 = parent2[:point] + parent1[point:]
-        # Adjust the child chromosomes to maintain the lattice constraints
-        # ...
         return child1, child2
     else:
         return parent1, parent2
@@ -230,13 +169,11 @@ def mutate(chromosome, mutation_rate, lattice):
     for i in range(len(chromosome)):
         if random.random() < mutation_rate:
             chromosome[i] = 1 - chromosome[i]
-            # Adjust the mutated chromosome to maintain the lattice constraints
-            # ...
     return chromosome
 
 def solve_scp_fca(formal_context, pop_size=100, max_gens=100, crossover_rate=0.8, mutation_rate=0.1):
     num_points = 10
-    lattice = Lattice(formal_context.lattice.generate_points(num_points))
+    lattice = LPStructure(points)
     subsets = {point: formal_context.relations[point] for point in lattice.points}  # Initialize subsets here
     population = generate_initial_population(pop_size, len(subsets))  # Use the length of subsets
     for gen in range(max_gens):
@@ -258,7 +195,13 @@ def solve_scp_fca(formal_context, pop_size=100, max_gens=100, crossover_rate=0.8
 # Пример использования
 points = [(x, y) for x in range(10) for y in range(10)]
 relations = {point: set([f'{x}{y}' for x in range(3) for y in range(3)]) for point in points}
-lattice = Lattice(points)
-formal_context = FormalContext(lattice, relations)
+formal_context = sequences = [
+    ['A', 'B', 'C'],
+    ['A', 'B', 'D'],
+    ['B', 'C', 'D'],
+    ['A', 'C', 'D'],
+    ['A', 'B', 'C', 'D']
+]  # Import the formal_context module and define it here
+lattice = LPStructure(points)
 solution = solve_scp_fca(formal_context)
 print("Best solution:", solution)
