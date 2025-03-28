99cfa2641dd165876b52b526731b0e1588337fd8
andrejstrogonov <strogonovandrej@hotmail.com>
2025-03-28 21:23:04 +0300

    Fixed algorithm

--- scp_final_lpstructure.py
+++ scp_final_lpstructure.py
@@ -1,5 +1,5 @@
-import random
-import unittest
+
+import random  # Import the entire random module instead of just the random function
 
 
 class LPStructure:
@@ -71,9 +71,11 @@ class SCPAlgorithm:
         self.generations = generations
 
     def generate_individual(self):
+        # Generate an individual as a list of integers
         individual = []
         for _ in range(self.lp_structure.eLengthItems):
-            item = [random.choice([0, 1]) for _ in range(self.lp_structure.eItemBitSize)]
+            # Generate a random integer with appropriate bit size
+            item = random.randint(0, (1 << self.lp_structure.eItemBitSize) - 1)
             individual.append(item)
         return individual
 
@@ -82,93 +84,67 @@ class SCPAlgorithm:
         return population
 
     def fitness(self, individual):
-        # Flatten the individual list
-        flat_individual = [bit for item in individual for bit in item]
+        # Calculate fitness based on the number of bits set to 1
         score = 0
-        for i in range(self.lp_structure.eLengthItems):
-            if self.lp_structure.isON(flat_individual, i):
+        for i in range(len(individual) * self.lp_structure.eItemBitSize):
+            if self.lp_structure.isON(individual, i):
                 score += 1
         return score
 
     def select(self, population):
         scored = [(self.fitness(individual), individual) for individual in population]
-        scored.sort()
+        scored.sort(reverse=True)  # Higher fitness is better
         ranked = [individual for (score, individual) in scored]
         return ranked[:int(0.2 * len(ranked))]  # Select top 20%
 
     def crossover(self, parent1, parent2):
-        child1 = [item[:] for item in parent1]  # Deep copy of parent1
-        child2 = [item[:] for item in parent2]  # Deep copy of parent2
-        for i in range(self.lp_structure.eLengthItems):
-            self.lp_structure.lJoin(child1[i], parent2[i])
-            self.lp_structure.lJoin(child2[i], parent1[i])
+        child1 = parent1.copy()  # Copy parent1
+        child2 = parent2.copy()  # Copy parent2
+
+        # Create temporary lists for lJoin operation
+        temp1 = child1.copy()
+        temp2 = child2.copy()
+
+        # Perform crossover using lJoin
+        self.lp_structure.lJoin(child1, parent2)
+        self.lp_structure.lJoin(child2, parent1)
+
         return child1, child2
 
     def mutate(self, individual):
-        for i in range(len(individual)):
+        mutated = individual.copy()
+        for i in range(len(mutated)):
             if random.random() < 0.01:  # 1% chance of mutation
-                individual[i] = [random.choice([0, 1]) for _ in range(self.lp_structure.eItemBitSize)]
-        return individual
+                # Flip a random bit
+                bit_position = random.randint(0, self.lp_structure.eItemBitSize - 1)
+                mutated[i] ^= (1 << bit_position)
+        return mutated
 
     def evolve(self):
         population = self.generate_population()
         for _ in range(self.generations):
             selected = self.select(population)
-            next_generation = selected[:]
+            next_generation = selected.copy()
+
             while len(next_generation) < self.population_size:
-                parents = random.sample(selected, 2)
-                children = self.crossover(parents[0], parents[1])
-                next_generation.extend(children)
+                if len(selected) >= 2:
+                    parent1, parent2 = random.sample(selected, 2)
+                    child1, child2 = self.crossover(parent1, parent2)
+                    next_generation.append(child1)
+                    if len(next_generation) < self.population_size:
+                        next_generation.append(child2)
+                else:
+                    # If not enough selected individuals, add a new random one
+                    next_generation.append(self.generate_individual())
+
             population = [self.mutate(individual) for individual in next_generation]
+
         best_individual = self.select(population)[0]
         return best_individual
 
-# Пример использования
+
+# Example usage
 lp_structure = LPStructure(10, 8)
 scp_algorithm = SCPAlgorithm(lp_structure)
 best_individual = scp_algorithm.evolve()
 print("Best Individual:", best_individual)
-
-class TestSCPAlgorithm(unittest.TestCase):
-    def setUp(self):
-        self.lp = LPStructure(3, 8)
-        self.scp = SCPAlgorithm(self.lp, population_size=10, generations=5)
-
-    def test_generate_individual(self):
-        individual = self.scp.generate_individual()
-        self.assertEqual(len(individual), 3)
-        for item in individual:
-            self.assertEqual(len(item), 8)
-
-    def test_generate_population(self):
-        population = self.scp.generate_population()
-        self.assertEqual(len(population), 10)
-
-    def test_fitness(self):
-        individual = [[0b10000000, 0, 0], [0, 0, 0], [0, 0, 0]]
-        score = self.scp.fitness(individual)
-        self.assertEqual(score, 1)
-
-    def test_select(self):
-        population = self.scp.generate_population()
-        selected = self.scp.select(population)
-        self.assertEqual(len(selected), 2)
-
-    def test_crossover(self):
-        parent1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        parent2 = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-        child1, child2 = self.scp.crossover(parent1, parent2)
-        self.assertEqual(len(child1), 3)
-        self.assertEqual(len(child2), 3)
-
-    def test_mutate(self):
-        individual = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        mutated = self.scp.mutate(individual)
-        self.assertEqual(len(mutated), 3)
-
-    def test_evolve(self):
-        best_individual = self.scp.evolve()
-        self.assertEqual(len(best_individual), 3)
-
-if __name__ == '__main__':
-    unittest.main()
